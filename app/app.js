6f587894f7cba428c5099fd792695ada20b6ee05
stevelyall <stevenlyall@gmail.com>
2016-04-10 15:04:36 -0700

    add missing ng-environment dependency

--- app/app.js
+++ app/app.js
@@ -1,18 +1,40 @@
 angular.module('app', [
-  'ngRoute',
-  'app.authService',
-  'app.login'
+    'ngRoute',
+    'environment',
+    'app.authService',
+    'app.login'
 
-]).config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/login'});
-}]).run(function ($rootScope, $location, authService) {
-  $rootScope.$on("$routeChangeStart", function (event, next, current) {
-    if (!authService.isUserAuthenticated()) {
-      // if not logged in, show login
-      if (next.templateUrl === "views/login/login.html") {
-      } else {
-        $location.path("/home");
+  ])
+  .config([
+    '$routeProvider',
+    'envServiceProvider',
+    function ($routeProvider, envServiceProvider) {
+      $routeProvider.otherwise({redirectTo: '/login'});
+
+      envServiceProvider.config({
+        domains: {
+          development: ["localhost"],
+          production: ["24.70.42.226"]
+        },
+        vars: {
+          development: {
+            apiUrl: "//localhost:5000/api"
+          },
+          production: {
+            apiUrl: "//kidsapp-api.herokuapp.com/api"
+          }
+        }
+      });
+
+    }])
+  .run(function ($rootScope, $location, authService) {
+    $rootScope.$on("$routeChangeStart", function (event, next, current) {
+      if (!authService.isUserAuthenticated()) {
+        // if not logged in, show login
+        if (next.templateUrl === "views/login/login.html") {
+        } else {
+          $location.path("/home");
+        }
       }
-    }
+    });
   });
-});
