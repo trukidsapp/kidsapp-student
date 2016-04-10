3d0eef32860e071cfb444a67a2633caab9aaeedc
stevelyall <stevenlyall@gmail.com>
2016-04-10 14:58:33 -0700

    app.js redirect to login

--- app/app.js
+++ app/app.js
@@ -5,4 +5,14 @@ angular.module('app', [
 
 ]).config(['$routeProvider', function ($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/login'});
-}]);
+}]).run(function ($rootScope, $location, authService) {
+  $rootScope.$on("$routeChangeStart", function (event, next, current) {
+    if (!authService.isUserAuthenticated()) {
+      // if not logged in, show login
+      if (next.templateUrl === "views/login/login.html") {
+      } else {
+        $location.path("/home");
+      }
+    }
+  });
+});
